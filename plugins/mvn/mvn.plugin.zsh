5c2684c8da5a592b057025e89b208517e85f2d1c
kubamarchwicki <kuba@marchwicki.pl>
2014-06-01 21:01:01 +0200

    mvn: add dynamic profile support to mvn completion
    * Maven profiles for current pom.xml file
    * Maven profiles for pom hierarchy
    * Ommiting comments in profiles

--- plugins/mvn/mvn.plugin.zsh
+++ plugins/mvn/mvn.plugin.zsh
@@ -68,156 +68,66 @@ alias mvntc='mvn tomcat:run'
 alias mvntc7='mvn tomcat7:run'
 alias mvn-updates='mvn versions:display-dependency-updates'
 
-		# jetty
-		jetty:run jetty:run-exploded
-		# cargo
-		cargo:start cargo:run cargo:stop cargo:deploy cargo:undeploy cargo:help
-		# jboss
-		jboss:start jboss:stop jboss:deploy jboss:undeploy jboss:redeploy
-		# tomcat
-		tomcat:start tomcat:stop tomcat:deploy tomcat:undeploy tomcat:redeploy
-		# tomcat6
-		tomcat6:run tomcat6:run-war tomcat6:run-war-only tomcat6:stop tomcat6:deploy tomcat6:undeploy
-		# tomcat7
-		tomcat7:run tomcat7:run-war tomcat7:run-war-only tomcat7:deploy
-		# tomee
-		tomee:run tomee:run-war tomee:run-war-only tomee:stop tomee:deploy tomee:undeploy
-		# spring-boot
-		spring-boot:run spring-boot:repackage
-		# exec
-		exec:exec exec:java
-		# versions
-		versions:display-dependency-updates versions:display-plugin-updates versions:display-property-updates versions:update-parent versions:update-properties versions:update-child-modules versions:lock-snapshots versions:unlock-snapshots versions:resolve-ranges versions:set versions:use-releases versions:use-next-releases versions:use-latest-releases versions:use-next-snapshots versions:use-latest-snapshots versions:use-next-versions versions:use-latest-versions versions:commit versions:revert
-		# scm
-		scm:add scm:bootstrap scm:branch scm:changelog scm:check-local-modification scm:checkin scm:checkout scm:diff scm:edit scm:export scm:list scm:remove scm:status scm:tag scm:unedit scm:update scm:update-subprojects scm:validate
-		# buildnumber
-		buildnumber:create buildnumber:create-timestamp buildnumber:help buildnumber:hgchangeset
+#realpath replacement for iOS - not always present
+function _realpath {
+	if [[ -f "$1" ]]
+	then
+		# file *must* exist
+		if cd "$(echo "${1%/*}")" &>/dev/null
+		then
+			# file *may* not be local
+			# exception is ./file.ext
+			# try 'cd .; cd -;' *works!*
+			local tmppwd="$PWD"
+			cd - &>/dev/null
+		else
+			# file *must* be local
+			local tmppwd="$PWD"
+		fi
+	else
+		# file *cannot* exist
+		return 1 # failure
+	fi
 
-		# war
-		war:war war:exploded war:inplace war:manifest
-		# ear
-		ear:ear ear:generate-application-xml
-		# ejb
-		ejb:ejb
-		# android
-		android:apk android:apklib android:deploy android:deploy-dependencies android:dex android:emulator-start android:emulator-stop android:emulator-stop-all android:generate-sources android:help android:instrument android:manifest-update android:pull android:push android:redeploy android:run android:undeploy android:unpack android:version-update android:zipalign android:devices
-		# nexus
-		nexus:staging-list nexus:staging-close nexus:staging-drop nexus:staging-release nexus:staging-build-promotion nexus:staging-profiles-list nexus:settings-download
-		# repository
-		repository:bundle-create repository:bundle-pack repository:help
+	# reassemble realpath
+	echo "$tmppwd"/"${1##*/}"
+	return 1 #success
+}
 
-		# sonar
-		sonar:sonar
-		# license
-		license:format license:check
-		# hibernate3
-		hibernate3:hbm2ddl hibernate3:help
-		# liquibase
-		liquibase:changelogSync liquibase:changelogSyncSQL liquibase:clearCheckSums liquibase:dbDoc liquibase:diff liquibase:dropAll liquibase:help liquibase:migrate liquibase:listLocks liquibase:migrateSQL liquibase:releaseLocks liquibase:rollback liquibase:rollbackSQL liquibase:status liquibase:tag liquibase:update liquibase:updateSQL liquibase:updateTestingRollback
-		# flyway
-		flyway:clean flyway:history flyway:init flyway:migrate flyway:status flyway:validate
-		# gwt
-		gwt:browser gwt:clean gwt:compile gwt:compile-report gwt:css gwt:debug gwt:eclipse gwt:eclipseTest gwt:generateAsync gwt:help gwt:i18n gwt:mergewebxml gwt:resources gwt:run gwt:sdkInstall gwt:source-jar gwt:soyc gwt:test
-		# asciidoctor
-		asciidoctor:process-asciidoc asciidoctor:auto-refresh asciidoctor:http asciidoctor:zip
-		# compiler
-		compiler:compile compiler:testCompile
-		# resources
-		resources:resources resources:testResources resources:copy-resources
-		# verifier
-		verifier:verify
-		# jar
-		jar:jar jar:test-jar
-		# rar
-		rar:rar
-		# acr
-		acr:acr
-		# shade
-		shade:shade
-		# changelog
-		changelog:changelog changelog:dev-activity changelog:file-activity
-		# changes
-		changes:announcement-mail changes:announcement-generate changes:changes-check changes:changes-validate changes:changes-report changes:jira-report changes:trac-report changes:github-report
-		# doap
-		doap:generate
-		# docck
-		docck:check
-		# jdeps
-		jdeps:jdkinternals jdeps:test-jdkinternals
-		# linkcheck
-		linkcheck:linkcheck
-		# project-info-reports
-		project-info-reports:cim project-info-reports:dependencies project-info-reports:dependency-convergence project-info-reports:dependency-info project-info-reports:dependency-management project-info-reports:distribution-management project-info-reports:help project-info-reports:index project-info-reports:issue-tracking project-info-reports:license project-info-reports:mailing-list project-info-reports:modules project-info-reports:plugin-management project-info-reports:plugins project-info-reports:project-team project-info-reports:scm project-info-reports:summary
-		# surefire-report
-		surefire-report:failsafe-report-only surefire-report:report surefire-report:report-only
-		# invoker
-		invoker:install invoker:integration-test invoker:verify invoker:run
-		# jarsigner
-		jarsigner:sign jarsigner:verify
-		# patch
-		patch:apply
-		# pdf
-		pdf:pdf
-		# plugin
-		plugin:descriptor plugin:report plugin:updateRegistry plugin:addPluginArtifactMetadata plugin:helpmojo
-		# remote-resources
-		remote-resources:bundle remote-resources:process
-		# scm-publish
-		scm-publish:help scm-publish:publish-scm scm-publish:scmpublish
-		# stage
-		stage:copy
-		# toolchain
-		toolchain:toolchain
+function __pom_hierarchy {
+	local file=`_realpath "pom.xml"`
+	POM_HIERARCHY+=("~/.m2/settings.xml")
+	POM_HIERARCHY+=("$file")
+	while [ -n "$file" ] && grep -q "<parent>" $file; do
+		##look for a new relativePath for parent pom.xml
+		local new_file=`grep -e "<relativePath>.*</relativePath>" $file | sed 's/.*<relativePath>//' | sed 's/<\/relativePath>.*//g'`
 
-		# options
-		"-Dmaven.test.skip=true" -DskipTests -DskipITs -Dmaven.surefire.debug -DenableCiProfile "-Dpmd.skip=true" "-Dcheckstyle.skip=true" "-Dtycho.mode=maven" "-Dmaven.test.failure.ignore=true" "-DgroupId=" "-DartifactId=" "-Dversion=" "-Dpackaging=jar" "-Dfile="
+		## <parent> is present but not defined. Asume ../pom.xml
+		if [ -z "$new_file" ]; then
+			new_file="../pom.xml"
+		fi
 
-		# arguments
-		-am --also-make
-		-amd --also-make-dependents-am
-		-B --batch-mode
-		-b --builder
-		-C --strict-checksums
-		-c --lax-checksums
-		-cpu --check-plugin-updates
-		-D --define
-		-e --errors
-		-emp --encrypt-master-password
-		-ep --encrypt-password
-		-f --file
-		-fae --fail-at-end
-		-ff --fail-fast
-		-fn --fail-never
-		-gs --global-settings
-		-gt --global-toolchains
-		-h --help
-		-l --log-file
-		-llr --legacy-local-repository
-		-N --non-recursive
-		-npr --no-plugin-registry
-		-npu --no-plugin-updates
-		-nsu --no-snapshot-updates
-		-o --offline
-		-P --activate-profiles
-		-pl --projects
-		-q --quiet
-		-rf --resume-from
-		-s --settings
-		-t --toolchains
-		-T --threads
-		-U --update-snapshots
-		-up --update-plugins
-		-v --version
-		-V --show-version
-		-X --debug
-
-		cli:execute cli:execute-phase
-		archetype:generate generate-sources
-		cobertura:cobertura
-		-Dtest=$(if [ -d ./src/test/java ] ; then find ./src/test/java -type f -name '*.java' | grep -v svn | sed 's?.*/\([^/]*\)\..*?-Dtest=\1?' ; fi)
-		-Dit.test=$(if [ -d ./src/test/java ] ; then find ./src/test/java -type f -name '*.java' | grep -v svn | sed 's?.*/\([^/]*\)\..*?-Dit.test=\1?' ; fi)
+		## if file exists continue else break
+		new_pom=`_realpath "${file%/*}/$new_file"`
+		if [ -n "$new_pom" ]; then
+			file=$new_pom
+		else
+			break
+		fi
+		POM_HIERARCHY+=("$file")
+	done
+}
 
 function listMavenCompletions {
+	POM_HIERARCHY=()
+	__pom_hierarchy
+
+	profiles=()
+	#current pom profiles
+	for item in ${POM_HIERARCHY[*]}; do
+		profiles=($profiles `[ -e $item ] && cat $item | sed 's/<!--.*-->//' | sed '/<!--/,/-->/d' | grep -e "<profile>" -A 1 | grep -e "<id>.*</id>" | sed 's?.*<id>\(.*\)<\/id>.*?-P\1?'`)
+	done
+
 	reply=(
 		# common lifecycle
 		clean initialize process-resources compile process-test-resources test-compile test package verify install deploy site
@@ -426,7 +336,12 @@ function listMavenCompletions {
 		cobertura:cobertura
 		-Dtest=$(if [ -d ./src/test/java ] ; then find ./src/test/java -type f -name '*.java' | grep -v svn | sed 's?.*/\([^/]*\)\..*?-Dtest=\1?' ; fi)
 		-Dit.test=$(if [ -d ./src/test/java ] ; then find ./src/test/java -type f -name '*.java' | grep -v svn | sed 's?.*/\([^/]*\)\..*?-Dit.test=\1?' ; fi)
+
+		$profiles
 	)
+
+	unset POM_HIERARCHY
+	unset profiles
 }
 
 compctl -K listMavenCompletions mvn mvnw
