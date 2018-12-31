1c24b234781bd4ed6ae94ede1e288db40146dcd8
Alexander Huynh <ahrex@users.noreply.github.com>
2018-12-31 14:15:14 -0500

    Provide even spacing between marks (#7456)
    Before, when typing the `marks` command, longer mark keys would cause
    the tabs to spill over to the next tab stop, like so:
    
        rc      -> /home/ahlex/.rc
        repos   -> /home/ahlex/repos
        a-longer-string         -> /tmp
    
    Implement better key display by running through all of the marks twice,
    once to get the longest key length, and the second time to format
    everything according to that length:
    
                     rc -> /home/ahlex/.rc
                  repos -> /home/ahlex/repos
        a-longer-string -> /tmp

--- plugins/jump/jump.plugin.zsh
+++ plugins/jump/jump.plugin.zsh
@@ -38,8 +38,8 @@ marks() {
 	for link in $MARKPATH/*(@); do
 		local markname="$fg[cyan]${link:t}$reset_color"
 		local markpath="$fg[blue]$(readlink $link)$reset_color"
-		printf "%s\t" $markname
-		printf -- "-> %s \t\n" $markpath
+		printf -- "$printf_markname_template" "$markname"
+		printf -- "-> %s\n" "$markpath"
 	done
 }
 
