c6f5d8f665b93e2c27d8d49496d6269439061ca9
Anton Stamenov <lidow@users.noreply.github.com>
2018-12-31 21:15:56 +0200

    aws_porfiles regex is catching more lines than profiles, thus breaking completion (#7469)

--- plugins/aws/aws.plugin.zsh
+++ plugins/aws/aws.plugin.zsh
@@ -34,7 +34,7 @@ function asp {
 }
 
 function aws_profiles {
-  reply=($(grep profile "${AWS_CONFIG_FILE:-$HOME/.aws/config}"|sed -e 's/.*profile \([a-zA-Z0-9_\.-]*\).*/\1/'))
+  reply=($(grep '\[profile' "${AWS_CONFIG_FILE:-$HOME/.aws/config}"|sed -e 's/.*profile \([a-zA-Z0-9_\.-]*\).*/\1/'))
 }
 
 if [ "$SHOW_AWS_PROMPT" != false ]; then
